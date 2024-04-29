9d1fe809f2f42a2a347d5087c8b7c65d2b5af637
odenra <ilkerkaraoden@gmail.com>
2024-04-29 16:36:24 +0300

    navbar is done bitch

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,31 @@
-import "./navbar.scss"
+import "./navbar.scss";
 
 const Navbar = () => {
   return (
-    <div className="navbar">Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="" />
+        <span>admin</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img
+            src="public/userp.png"
+            alt=""
+          />
+          <span>İlker</span>
+        </div>
+        <img src="/settings.svg" alt="" className="icon" />
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;

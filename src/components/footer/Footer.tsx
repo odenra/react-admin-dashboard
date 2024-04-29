6da3ff1fa76c91d4b04b04b4fff2edb660a897fa
odenra <ilkerkaraoden@gmail.com>
2024-04-29 16:46:23 +0300

    yes footer also done

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,10 @@ import "./footer.scss"
 
 const Footer = () => {
   return (
-    <div className="footer">Footer</div>
+    <div className="footer">
+      <span>admin</span>
+      <span>© İ Dev Admin Dashboard</span>
+    </div>
   )
 }
 
